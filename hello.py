a60cfd6eec29970be3e91a3afb7d6e0bbaf44585
vld00 <leodoriandesign@gmail.com>
2023-09-29 17:48:40 +0200

    Create hello.py
    1st commit

---
+++ hello.py
@@ -0,0 +1,19 @@
+print("Hello, World ! ")
+
+name = input("Name: ")
+
+print("What is your surname?  " + name)
+
+surname = input("Surname: ")
+
+print(f'Welcome ' + (name )+(" ")+(surname)+'!')
+
+n = int(input("What is your favorite number? : "))
+
+
+if n < 10:
+    print("your favorite number is very little ")
+elif n > 5:
+    print("--")
+else:
+    print("n is ")
